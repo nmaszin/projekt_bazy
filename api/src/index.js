7dece06b048f2bd9144aab0d9ddf46efc119f234
qwercik <erykandrzejewski@gmail.com>
2021-10-23 23:44:40 +0200

    Split main index.js file into routes.js and config.js modules

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,16 @@
 import express from 'express'
-
-const port = 8080
+import config from './config.js'
+import router from './routes.js'
 
 const app = express()
-app.get('/', (req, res) => {
-    res.send({
-        message: 'Hello world'
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', router)
+app.use('/', (req, res) => {
+    res.status(404).send({
+        message: 'Unknown route'
     })
 })
 
-app.listen(port, () => {
-    console.log(`Running on https://localhost:${port}`)
-})
+app.listen(config.port, () => console.log(`Running on https://localhost:${config.port}`))
