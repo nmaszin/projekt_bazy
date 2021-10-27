9cc6c3b17da2f9feb675ceed4bc159ef5170767a
Konrad Bankiewicz <kondziobank@gmail.com>
2021-10-27 23:22:57 +0200

    Pierwszy etap front-end

---
+++ web/src/layouts/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Header from './Header'
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+
+        <header>
+          { <Header /> }
+        </header>
+
+        <main>
+          <aside>
+            { <Navigation />}
+          </aside>
+          
+          <section className="page">
+            {<Page />}
+          </section>
+        </main>
+
+        <footer>
+          <footer>{<Footer />}</footer>
+        </footer>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
