400c3d63415ff74d0d7230c869a30379bce92dfa
qwercik <erykandrzejewski@gmail.com>
2021-11-01 23:15:10 +0100

    Connect to database

---
+++ api/src/db.js
@@ -0,0 +1,9 @@
+import mysql from 'mysql2/promise'
+
+export default async () => await mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+})
--- api/src/index.js
+++ api/src/index.js
@@ -1,18 +1,23 @@
 import express from 'express'
 import endpoints from '@/endpoints'
+import database from '@/db'
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+(async () => {
+    const db = await database()
+    const [result, _] = await db.query('SELECT 1 + 1 AS result')
+    console.log(result)
 
-app.use(endpoints)
-app.use('/', (req, res) => {
-    res.status(404).send({
-        message: 'Unknown route'
+    const app = express()
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: false }))
+
+    app.use(endpoints)
+    app.use('/', (req, res) => {
+        res.status(404).send({
+            message: 'Unknown route'
+        })
     })
-})
 
-console.log(process.env.MYSQL_HOST)
-
-const port = process.env.PORT
-app.listen(port, () => console.log(`Running on http://localhost:${port}`))
+    const port = process.env.PORT
+    app.listen(port, () => console.log(`Running on http://localhost:${port}`))
+})()
