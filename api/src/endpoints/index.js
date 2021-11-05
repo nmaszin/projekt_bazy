98a0c61e7ded03871b314798ea6cda7eb580cd1f
qwercik <erykandrzejewski@gmail.com>
2021-11-05 23:30:33 +0100

    Load endpoints files dynamically

--- api/src/endpoints/index.js
+++ api/src/endpoints/index.js
@@ -1,21 +1,13 @@
 import fs from 'fs'
 import path from 'path'
 import { Router } from 'express'
+import { loadDirectoryModules } from '@/utils/directoryLoader';
 
 // The following function load all endpoints routers and bind them into a new one
 // I assume that all files in current directory (except current file) are endpoints files
 export async function loadAllEndpoints() {
-    const currentFile = path.basename(__filename);
-    const endpointsDirectory = path.dirname(__filename)
-
-    const endpointFiles = fs.readdirSync(endpointsDirectory)
-        .filter(filename => filename != currentFile)
-
     const router = Router()
-    for (const endpointFile of endpointFiles) {
-        const endpointRouter = (await import(`./${endpointFile}`)).default
-        router.use(endpointRouter)
-    }
-
+    const routers = await loadDirectoryModules(__dirname, ['index.js'])
+    routers.forEach(r => router.use(r))
     return router
 }
